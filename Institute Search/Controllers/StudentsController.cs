3d9a28c1b4fe32569685efe35a9115280fd59c66
Alex Gilmer <alex.gilmer@mitt.ca>
2022-12-21 14:01:50 -0600

    Add StudentsController

---
+++ Institute Search/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LINQ_test.Classes;
+
+namespace Institute_Search.Controllers
+{
+    public class StudentsController : Controller
+    {
+        Institute Repo = Institute.GetInstance();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View(Repo.Students);
+        }
+    }
+}
