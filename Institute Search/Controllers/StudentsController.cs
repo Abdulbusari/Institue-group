970930e7b35f66b870ed7d50595a2071928079c0
Abdulahi Busari <Abdullahi.busary@gmail.om>
2023-01-06 11:26:10 -0600

    hi

--- Institute Search/Controllers/StudentsController.cs
+++ Institute Search/Controllers/StudentsController.cs
@@ -18,16 +18,44 @@ namespace Institute_Search.Controllers
             return View(Repo.Students);
         }
 
+
         public IActionResult Search()
         {
-            return View();
+            return View(new StudentID());
         }
 
+
         [HttpPost]
-        public IActionResult Search(/* you can change the parameters */ StudentSearchForm query)
+        public IActionResult Search(StudentID sd)
         {
-            // var result = Repo.......
-            return View(/* result */);
+            sd.results = Repo.Students.ToList();
+            if (!string.IsNullOrWhiteSpace(sd.range))
+            {
+                if (sd.range.Contains("<"))
+                {
+                    sd.range = sd.range.Replace('<', ' ');
+                    sd.results = sd.results.Where(s => s.Id <= int.Parse(sd.range)).ToList();
+                }
+                else if (sd.range.Contains(">"))
+                {
+                    sd.range = sd.range.Replace('>', ' ');
+                    sd.results = sd.results.Where(s => s.Id >= int.Parse(sd.range)).ToList();
+                }
+                else if (sd.range.Contains("-"))
+                {
+                    sd.range = sd.range.Replace('-', ' ');
+                    List<string> ranges = sd.range.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+                    sd.results = sd.results.Where(s => s.Id >= int.Parse(ranges[0]) && s.Id <= int.Parse(ranges[1])).ToList();
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(sd.name))
+            {
+                 string[] qs = sd.name.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                 IEnumerable<Student> sts = sd.results.Where(s => qs.All(t => $"{s.FirstName} {s.LastName}".ToLower().Contains(t)));
+                sd.results = sts.ToList();
+            }
+            return View(sd);
         }
     }
 }
---
+++ Institute Search/Models/StudentID.cs
@@ -0,0 +1,15 @@
+﻿using LINQ_test.Classes;
+using System.Globalization;
+
+namespace Institute_Search.Models
+{
+    public class StudentID
+    {
+        public string range { get; set; }
+
+        public List<Student> results;
+        public string name { get; set; }
+
+
+    }
+}
